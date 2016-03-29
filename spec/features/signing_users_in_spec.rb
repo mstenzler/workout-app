07e33c2ed9c131027acf863469d22ee3b14de1ff
Mike Stenzler <mike@nexuscafe.com>
2016-03-29 17:22:42 -0400

    Add user signin feature

--- spec/features/signing_users_in_spec.rb
+++ spec/features/signing_users_in_spec.rb
@@ -12,10 +12,10 @@ RSpec.feature "Signing users in" do
     click_link "Sign in"
     fill_in "Email", with: @john.email
     fill_in "Password", with: @john.password
-    click_link "Log in"
+    click_button "Log in"
 
-    expect(page).to have_content("Signed in succesfully")
-    expext(page).to have_content("Signed in as #{@john.email}")
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("Signed in as #{@john.email}")
   end
   
 end
