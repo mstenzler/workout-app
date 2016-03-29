94b915e1b160afc4f5abd0e4177a5ded0b9ed696
Mike Stenzler <mike@nexuscafe.com>
2016-03-29 16:52:17 -0400

    Setup user signup

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Users Signup" do
 
   scenario "with valid creadentials" do
     visit "/"
-    
+
     click_link "Sign up"
 
     fill_in "Email", with: "john@example.com"
@@ -12,6 +12,6 @@ RSpec.feature "Users Signup" do
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
-    expect(page).to have_content('You have signup up successfully')
+    expect(page).to have_content('You have signed up successfully')
   end  
 end
