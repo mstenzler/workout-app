3cc736e67e49a0664864d073a5ef90d233e2a14d
Mike Stenzler <mike@nexuscafe.com>
2016-04-05 13:51:58 -0400

    Add frienship and follow-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ActiveRecord::Base
   has_many :exercises
   validates :first_name, presence: true
   validates :last_name, presence: true
-  self.per_page = 2
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
+  self.per_page = 10
 
   def full_name
     "#{first_name} #{last_name}"
@@ -21,6 +23,10 @@ class User < ActiveRecord::Base
       where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ? or last_name LIKE ?',
         "%#{names_array[0]}%","%#{names_array[1]}%","%#{names_array[0]}%","%#{names_array[1]}%").order(:first_name)
     end
+  end
+
+  def follows_or_same?(new_friend)
+    friendships.map(&:friend).include?(new_friend) || self == new_friend
   end
    
 end
